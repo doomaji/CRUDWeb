7d6a2c71062467f6a7cc04d9d04940fe3044a387
doomaji <doomaji@gmail.com>
2025-12-22 22:25:46 +0900

    fifthCommit

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -2,9 +2,8 @@ package web.model;
 
 import javax.persistence.*;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+
 
 @Entity
 @Table(name = "users")
